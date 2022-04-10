a4bd7414c854d4b2e62c84a018ca835e0d92ffd3
steved <smddurbs@gmail.com>
2022-04-10 14:45:45 +1200

    Added text input functions

---
+++ app/src/main/java/com/example/noteapp/components/NoteComponents.kt
@@ -0,0 +1,85 @@
+package com.example.noteapp.components
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun NoteInputText(
+    modifier: Modifier = Modifier,
+    text: String,
+    Label: String,
+    maxLine: Int = 1,
+    onTextChange: (String) -> Unit,
+    onImeAction: () -> Unit = {}
+    ){
+
+    val keyBoardController = LocalSoftwareKeyboardController.current
+
+    TextField(
+        value = text,
+        onValueChange = onTextChange,
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color.Transparent),
+        maxLines = maxLine,
+        label = {Text(Label)},
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = {
+            onImeAction()
+            keyBoardController?.hide()
+        }),
+        modifier = modifier
+    )
+
+}
+
+
+@Composable
+fun NoteButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onCLick: () -> Unit,
+    enabled: Boolean = true
+){
+    Button(
+        onClick = onCLick,
+        shape = CircleShape,
+        enabled = enabled,
+        modifier = modifier) {
+
+        Text(text)
+
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/noteapp/screen/NoteScreen.kt
+++ app/src/main/java/com/example/noteapp/screen/NoteScreen.kt
@@ -1,2 +1,89 @@
 package com.example.noteapp.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.noteapp.R
+import com.example.noteapp.components.NoteButton
+import com.example.noteapp.components.NoteInputText
+
+@Composable
+fun NoteScreen(){
+
+    var title by remember {
+        mutableStateOf("")
+    }
+
+    var description by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(6.dp)) {
+
+        TopAppBar(
+            title = {
+                    Text(stringResource(id = R.string.app_name))
+            },
+            actions = {
+                Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "notifications icon")
+            },
+            backgroundColor = Color(0x75817FC2))
+        
+        //Content
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            
+            NoteInputText(
+                modifier = Modifier
+                    .padding(top = 10.dp, bottom = 8.dp),
+                text = title,
+                Label = "Title",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        })
+                            title = it
+                })
+
+            NoteInputText(
+                modifier = Modifier
+                    .padding(top = 10.dp, bottom = 20.dp),
+                text = description,
+                Label = "Add Note",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace()
+                        })
+                        description = it
+                })
+            
+            NoteButton(
+                text = "Save",
+                onCLick = { /*TODO*/ })
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun NotesScreenPreview(){
+    NoteScreen()
+}
